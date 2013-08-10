9f7397b226c633703dbfe80a741f7d7ad35dbcb5
kaestel <martin@hvadhedderde.com>
2013-08-10 02:33:20 +0200

    initial commit

---
+++ src/www/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- (c) & (p) think.dk 2009-2010 //-->
+	<!-- All material protected by copyrightlaws //-->
+	<meta name="google-site-verification" content="MbLL459QavncFr9ChNv5Gley9Upn1kxuKGOLLE1_IBA" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>kaestel.dk</title>
+	<script language="javascript" type="text/javascript" src="js/scripts.js"></script>
+	<style type="text/css">@import url(css/styles.css);</style>
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-17394677-1']);
+		_gaq.push(['_setDomainName', '.kaestel.dk']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+		  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
+</head>
+
+<body>
+
+<div id="page">
+	<div id="inner">
+		<h1>kaestel.dk</h1>
+		<p>Har du set at der findes en <a href="http://www.facebook.com/group.php?gid=54354123274">&quot;k&aelig;stel&quot;-gruppe p&aring; facebook</a>?</p>
+		<p>Hvis du hedder k&aelig;stel og er interesseret i en kaestel-email (dit navn @ kaestel.dk), s&aring; skriv til <script language="javascript" type="text/javascript">Util.otliam(&#x27;martin&#x27;, &#x27;kaestel.dk&#x27;)</script>. Hvis vi ikke bruger/skal bruge adressen laver vi gerne en gratis konto til dig.</p>
+		<p>Der er ogs&aring; mulighed for subdomains og lignende hvis det skulle v&aelig;re interessant (dog uden hosting), og hvis du har ideer til siden her, s&aring; lad mig endelig h&oslash;re fra dig.</p>
+		<p><em>Martin K&aelig;stel Nielsen</em></p>
+	</div>
+</div>
+
+</body>
+</html>
