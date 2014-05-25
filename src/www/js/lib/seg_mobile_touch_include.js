3ea5628124ca770b1102bd8bff02767cee462007
kaestel <kaestel@e-types.com>
2014-05-25 14:56:31 +0200

    schema test

--- src/www/js/lib/seg_mobile_touch_include.js
+++ src/www/js/lib/seg_mobile_touch_include.js
@@ -5,10 +5,4 @@ document.write('<script type="text/javascript" src="/js/lib/ga.js"></script>');
 document.write('<script type="text/javascript" src="/js/manipulator/src/u-googleanalytics.js"></script>');
 
 document.write('<script type="text/javascript" src="http://parentnode.local/js/lib/mobile_touch/i-page-mobile_touch.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/lib/mobile_touch/i-login-mobile_touch.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/lib/mobile_touch/i-generic-mobile_touch.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/lib/mobile_touch/i-article-mobile_touch.js"></script>');
-
-document.write('<script type="text/javascript" src="/js/lib/mobile_touch/i-wishlist-mobile_touch.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/mobile_touch/i-todolist-mobile_touch.js"></script>');
 
