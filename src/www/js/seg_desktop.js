adcb5535c2da2d4ca13afc52bffb72f589cfe733
kaestel <kaestel@e-types.com>
2014-07-19 00:20:22 +0200

    merged

--- src/www/js/seg_desktop.js
+++ src/www/js/seg_desktop.js
@@ -3183,7 +3183,7 @@ u.site_name = "Kæstel";
 /*ga.js*/
 u.ga_account = 'UA-17394677-1';
 u.ga_domain = 'kaestel.dk';
-
+u.gapi_key = 'AIzaSyDDcI1nS5XiY3pfMQnlGVU4Ev2aAQZ8Wog';
 
 /*u-googleanalytics.js*/
 if(u.ga_account) {
--- src/www/js/seg_mobile_light.js
+++ src/www/js/seg_mobile_light.js
@@ -14,7 +14,7 @@ if(!u || !Util) {
 /*ga.js*/
 u.ga_account = 'UA-17394677-1';
 u.ga_domain = 'kaestel.dk';
-
+u.gapi_key = 'AIzaSyDDcI1nS5XiY3pfMQnlGVU4Ev2aAQZ8Wog';
 
 /*u-googleanalytics.js*/
 if(u.ga_account) {
