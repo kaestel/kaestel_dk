c4bccc2fab1b6ed0eaf3f765be4b9fef71894df8
kaestel <kaestel@e-types.com>
2014-07-19 15:24:50 +0200

    fixed log order and added images

--- src/templates/geek/logs_tag.php
+++ src/templates/geek/logs_tag.php
@@ -16,7 +16,12 @@ $log_tags = $IC->getTags(array("context" => "log"));
 	<ul class="logs">
 <?		foreach($log_items as $item):
 			$item = $IC->extendItem($item, array("tags" => true)); ?>
-		<li class="log id:<?= $item["item_id"] ?>" itemscope itemtype="http://schema.org/blog">
+		<li class="log id:<?= $item["item_id"] ?> i:article" itemscope itemtype="http://schema.org/blog">
+
+<?			if($item["files"]): ?>
+			<div class="image image_id:<?= $item["item_id"] ?> format:<?= $item["files"] ?>"></div>
+<?			endif; ?>
+
 			<ul class="tags">
 				<li><a href="/geek/logs">Logs</a></li>
 <?			if($item["tags"]): ?>
