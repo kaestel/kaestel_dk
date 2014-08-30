e00ad2a39acd097d418083e41e7cbd271beb55fe
kaestel <kaestel@e-types.com>
2014-08-30 19:46:36 +0200

    Updated sql

--- src/config/db/item_page.sql
+++ src/config/db/item_page.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `SITE_DB`.`item_post` (
+CREATE TABLE `SITE_DB`.`item_page` (
   `id` int(11) NOT NULL auto_increment,
   `item_id` int(11) NOT NULL,
 
@@ -8,5 +8,5 @@ CREATE TABLE `SITE_DB`.`item_post` (
 
   PRIMARY KEY  (`id`),
   KEY `item_id` (`item_id`),
-  CONSTRAINT `item_post_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `item_page_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
--- src/config/db/item_page_mediae.sql
+++ src/config/db/item_page_mediae.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `SITE_DB`.`item_post_mediae` (
+CREATE TABLE `SITE_DB`.`item_page_mediae` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(11) NOT NULL,
 
@@ -11,5 +11,5 @@ CREATE TABLE `SITE_DB`.`item_post_mediae` (
 
   PRIMARY KEY (`id`),
   KEY `item_id` (`item_id`),
-  CONSTRAINT `item_post_mediae_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `item_page_mediae_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
