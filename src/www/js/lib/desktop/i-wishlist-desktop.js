427294f57dc7c4478ed9c8b45f52e9007a1f6852
kaestel <kaestel@e-types.com>
2014-08-31 01:00:28 +0200

    Work with inaccessible form

--- src/www/js/lib/desktop/i-wishlist-desktop.js
+++ src/www/js/lib/desktop/i-wishlist-desktop.js
@@ -90,42 +90,70 @@ Util.Objects["wishes"] = new function() {
 
 					// initialize forms
 					node.reserve_form = u.qs("li.reserve form", node);
-					u.f.init(node.reserve_form);
-					node.bn_reserve = u.qs("input[type=submit]", node.reserve_form);
-					node.bn_reserve.node = node;
-					u.e.click(node.bn_reserve)
-					node.bn_reserve.clicked = function(event) {
-						u.e.kill(event);
+					if(node.reserve_form) {
+						u.f.init(node.reserve_form);
+						node.bn_reserve = u.qs("input[type=submit]", node.reserve_form);
+						node.bn_reserve.node = node;
 
-						this.response = function(response) {
-							if(response.cms_status == "success") {
-								u.ac(this.node.actions, "reserved");
-							}
-							else {
-//								alert("server communication failed");
-							}
+						node.bn_reserve.over = function() {
+							this.org_text = this.value;
+							this.value = "Click to reserve";
 						}
-						u.request(this, this.form.action, {"method":this.form.method, "params":u.f.getParams(this.form)});
+						node.bn_reserve.out = function() {
+							this.value = this.org_text;
+						}
+						u.e.addEvent(node.bn_reserve, "mouseover", node.bn_reserve.over);
+						u.e.addEvent(node.bn_reserve, "mouseout", node.bn_reserve.out);
+
+						u.e.click(node.bn_reserve)
+						node.bn_reserve.clicked = function(event) {
+							u.e.kill(event);
+
+							this.response = function(response) {
+								if(response.cms_status == "success") {
+									u.ac(this.node.actions, "reserved");
+								}
+								else {
+	//								alert("server communication failed");
+								}
+							}
+							u.request(this, this.form.action, {"method":this.form.method, "params":u.f.getParams(this.form)});
+						}
 					}
+
 
 					node.unreserve_form = u.qs("li.unreserve form", node);
-					u.f.init(node.unreserve_form);
-					node.bn_unreserve = u.qs("input[type=submit]", node.unreserve_form);
-					node.bn_unreserve.node = node;
-					u.e.click(node.bn_unreserve)
-					node.bn_unreserve.clicked = function(event) {
-						u.e.kill(event);
+					if(node.unreserve_form) {
+						u.f.init(node.unreserve_form);
+						node.bn_unreserve = u.qs("input[type=submit]", node.unreserve_form);
+						node.bn_unreserve.node = node;
 
-						this.response = function(response) {
-							if(response.cms_status == "success") {
-								u.rc(this.node.actions, "reserved");
-							}
-							else {
-//								alert("server communication failed");
-							}
+						node.bn_unreserve.over = function() {
+							this.org_text = this.value;
+							this.value = "Click to make available";
 						}
-						u.request(this, this.form.action, {"method":this.form.method, "params":u.f.getParams(this.form)});
+						node.bn_unreserve.out = function() {
+							this.value = this.org_text;
+						}
+						u.e.addEvent(node.bn_unreserve, "mouseover", node.bn_unreserve.over);
+						u.e.addEvent(node.bn_unreserve, "mouseout", node.bn_unreserve.out);
+
+						u.e.click(node.bn_unreserve)
+						node.bn_unreserve.clicked = function(event) {
+							u.e.kill(event);
+
+							this.response = function(response) {
+								if(response.cms_status == "success") {
+									u.rc(this.node.actions, "reserved");
+								}
+								else {
+	//								alert("server communication failed");
+								}
+							}
+							u.request(this, this.form.action, {"method":this.form.method, "params":u.f.getParams(this.form)});
+						}
 					}
+
 				}
 			}
 
