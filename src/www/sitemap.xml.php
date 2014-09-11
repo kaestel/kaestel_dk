9adac16ca95de100f079e9b69cff5dbf6e467571
kaestel <kaestel@e-types.com>
2014-09-11 21:15:06 +0200

    done fixing

--- src/www/sitemap.xml.php
+++ src/www/sitemap.xml.php
@@ -5,79 +5,66 @@ if(isset($read_access) && $read_access) {
 }
 
 include_once($_SERVER["FRAMEWORK_PATH"]."/config/init.php");
-$query = new Query();
 $IC = new Item();
 
 print '<?xml version="1.0" encoding="UTF-8"?>';
-
-$items = $IC->getCompleteItem("combined-curated");
 ?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 	<url>
-		<loc>http://e-types.com/</loc>
-		<lastmod><?= date("Y-m-d", strtotime($items["modified_at"])) ?></lastmod>
+		<loc>http://kaestel.dk/</loc>
+		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/pages/front.php")) ?></lastmod>
 		<changefreq>weekly</changefreq>
 		<priority>1</priority>
+	</url>
+	<url>
+		<loc>http://kaestel.dk/plain</loc>
+		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/pages/plain.php")) ?></lastmod>
+		<changefreq>weekly</changefreq>
+		<priority>1</priority>
+	</url>
+	<url>
+		<loc>http://kaestel.dk/terms</loc>
+		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/pages/terms.php")) ?></lastmod>
+		<changefreq>monthly</changefreq>
+		<priority>0.3</priority>
 	</url>
 <?
 
 
-	// NEWS ITEMS
-	$items = $IC->getItems(array("itemtype" => "news", "status" => 1)); ?>
+	// LOG ITEMS
+	$items = $IC->getItems(array("itemtype" => "log", "status" => 1)); ?>
 	<url>
-		<loc>http://e-types.com/journal/</loc>
+		<loc>http://kaestel.dk/geek/logs</loc>
 		<lastmod><?= date("Y-m-d", strtotime($items[0]["modified_at"])) ?></lastmod>
 		<changefreq>daily</changefreq>
 		<priority>1</priority>
 	</url>
 <? foreach($items as $item): ?>
 	<url>
-		<loc>http://e-types.com/article/<?= $item["sindex"] ?></loc>
+		<loc>http://kaestel.dk/geek/logs/<?= $item["sindex"] ?></loc>
 		<lastmod><?= date("Y-m-d", strtotime($item["modified_at"])) ?></lastmod>
 		<changefreq>weekly</changefreq>
 		<priority>0.9</priority>
 	</url>
-<? endforeach; 
+<? endforeach;
 
 
+	// POST ITEMS
+	$items = $IC->getItems(array("itemtype" => "post", "status" => 1)); ?>
+	<url>
+		<loc>http://kaestel.dk/geek/posts</loc>
+		<lastmod><?= date("Y-m-d", strtotime($items[0]["modified_at"])) ?></lastmod>
+		<changefreq>daily</changefreq>
+		<priority>1</priority>
+	</url>
+<? foreach($items as $item): ?>
+	<url>
+		<loc>http://kaestel.dk/geek/posts/<?= $item["sindex"] ?></loc>
+		<lastmod><?= date("Y-m-d", strtotime($item["modified_at"])) ?></lastmod>
+		<changefreq>weekly</changefreq>
+		<priority>0.9</priority>
+	</url>
+<? endforeach; ?>
 
-	// STATIC PAGES
-?>
-	<url>
-		<loc>http://e-types.com/about</loc>
-		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/e-types/about.php")) ?></lastmod>
-		<changefreq>weekly</changefreq>
-		<priority>0.7</priority>
-	</url>
-	<url>
-		<loc>http://e-types.com/competences</loc>
-		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/e-types/competences.php")) ?></lastmod>
-		<changefreq>weekly</changefreq>
-		<priority>0.7</priority>
-	</url>
-	<url>
-		<loc>http://e-types.com/daily/about</loc>
-		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/daily/about.php")) ?></lastmod>
-		<changefreq>weekly</changefreq>
-		<priority>0.7</priority>
-	</url>
-	<url>
-		<loc>http://e-types.com/daily/services</loc>
-		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/daily/services.php")) ?></lastmod>
-		<changefreq>weekly</changefreq>
-		<priority>0.7</priority>
-	</url>
-	<url>
-		<loc>http://e-types.com/playtype</loc>
-		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/pages/playtype.php")) ?></lastmod>
-		<changefreq>weekly</changefreq>
-		<priority>0.7</priority>
-	</url>
-	<url>
-		<loc>http://e-types.com/contact</loc>
-		<lastmod><?= date("Y-m-d", filemtime(LOCAL_PATH."/templates/pages/contact.php")) ?></lastmod>
-		<changefreq>weekly</changefreq>
-		<priority>0.7</priority>
-	</url>
 </urlset>
