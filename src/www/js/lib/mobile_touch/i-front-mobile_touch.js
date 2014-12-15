87b4c84833ee56451eb375098c11ac01058480f2
kaestel <kaestel@e-types.com>
2014-12-15 12:55:50 +0300

    Frontpage adjustment

--- src/www/js/lib/mobile_touch/i-front-mobile_touch.js
+++ src/www/js/lib/mobile_touch/i-front-mobile_touch.js
@@ -1,12 +1,12 @@
 Util.Objects["front"] = new function() {
 	this.init = function(scene) {
 
-		var nodes = u.qsa("li.item h3", scene);
+		var nodes = u.qsa("li.item", scene);
 		var i, node
 		if(nodes) {
 			for(i = 0; node = nodes[i]; i++) {
 
-				u.ce(node, {"type":"link"});
+				u.ce(node, {"type":"link", "use":"h3 a"});
 
 			}
 		}
--- src/www/js/seg_mobile_touch.js
+++ src/www/js/seg_mobile_touch.js
@@ -3916,11 +3916,11 @@ Util.Objects["article"] = new function() {
 /*i-front-mobile_touch.js*/
 Util.Objects["front"] = new function() {
 	this.init = function(scene) {
-		var nodes = u.qsa("li.item h3", scene);
+		var nodes = u.qsa("li.item", scene);
 		var i, node
 		if(nodes) {
 			for(i = 0; node = nodes[i]; i++) {
-				u.ce(node, {"type":"link"});
+				u.ce(node, {"type":"link", "use":"h3 a"});
 			}
 		}
 	}
