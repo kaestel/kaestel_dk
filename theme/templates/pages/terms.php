dcfada6500470c0a3665855f3ec189910fa1355a
Martin Kæstel Nielsen <martin@think.dk>
2018-11-25 11:28:19 +0000

    Terms and plain dynamic pages

--- theme/templates/pages/terms.php
+++ theme/templates/pages/terms.php
@@ -1,101 +1,57 @@
+<?
+global $action;
+global $IC;
+
+$page_item = $IC->getItem(array("tags" => "page:terms", "extend" => array("mediae" => true, "tags" => true)));
+
+if($page_item) {
+	$this->sharingMetaData($page_item);
+}
+?>
 <div class="scene terms i:scene">
 
-	<div class="article" itemscope itemtype="http://schema.org/Article">
-		<h1 itemprop="headline">Cookies, privacy and terms of use</h1>
-	
-		<ul class="info">
-			<li class="published_at" itemprop="datePublished" content="<?= date("Y-m-d", filemtime(__FILE__)) ?>"><?= date("Y-m-d, H:i", filemtime(__FILE__)) ?></li>
-			<li class="modified_at" itemprop="dateModified" content="<?= date("Y-m-d", filemtime(__FILE__)) ?>"></li>
-			<li class="author" itemprop="author">Martin Kæstel Nielsen</li>
-			<li class="main_entity" itemprop="mainEntityOfPage" content="<?= SITE_URL."/terms" ?>"></li>
-			<li class="publisher" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
-				<ul class="publisher_info">
-					<li class="name" itemprop="name">kaestel.dk</li>
-					<li class="logo" itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
-						<span class="image_url" itemprop="url" content="<?= SITE_URL ?>/img/logo-large.png"></span>
-						<span class="image_width" itemprop="width" content="720"></span>
-						<span class="image_height" itemprop="height" content="405"></span>
-					</li>
-				</ul>
-			</li>
-			<li class="image_info" itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
-				<span class="image_url" itemprop="url" content="<?= SITE_URL ?>/img/logo-large.png"></span>
-				<span class="image_width" itemprop="width" content="720"></span>
-				<span class="image_height" itemprop="height" content="405"></span>
-			</li>
-		</ul>
+<? if($page_item && $page_item["status"]): 
+	$media = $IC->sliceMedia($page_item); ?>
+	<div class="article i:article id:<?= $page_item["item_id"] ?>" itemscope itemtype="http://schema.org/Article">
+
+		<? if($media): ?>
+		<div class="image item_id:<?= $page_item["item_id"] ?> format:<?= $media["format"] ?> variant:<?= $media["variant"] ?>"></div>
+		<? endif; ?>
 
 
+		<?= $HTML->articleTags($page_item, [
+			"context" => false
+		]) ?>
+
+
+		<h1 itemprop="headline"><?= $page_item["name"] ?></h1>
+
+		<? if($page_item["subheader"]): ?>
+		<h2 itemprop="alternativeHeadline"><?= $page_item["subheader"] ?></h2>
+		<? endif; ?>
+
+
+		<?= $HTML->articleInfo($page_item, "/terms", [
+			"media" => $media,
+			"sharing" => false
+		]) ?>
+
+
+		<? if($page_item["html"]): ?>
 		<div class="articlebody" itemprop="articleBody">
-			<p>
-				By using this website you accept the terms as specified below. If you cannot accept
-				these terms, you cannot use the website.
-			</p>
-			<p>
-				This website is owned  and maintained by Martin Kæstel Nielsen (in the 
-				following referred to as "Me", "I", "My", "Mine"), who is a Danish citizen, abiding Danish laws.
-			</p>
-	
-			<h2>Cookies</h2>
-			<p>
-				I love cookies and I only serve safe, quality cookies. No transfat or artificial sweetener. My cookies 
-				will do you no harm.
-			</p>
-			<p>
-				When you access my website I identify the type of browser you are using. I do this to serve the best
-				version of this website available. This information is saved on my server for the duration of your visit, 
-				and I need a cookie to link this information to your browser. That way I do not need to identify your 
-				browser every time you view a new page. When you leave the website, I delete the stored information.
-			</p>
-			<p>
-				Ironically the only long term cookie I save on your computer, is the one used to remember your cookie
-				and terms acceptance. This cookie is stored for 12 months and prevents the "Accept cookies" dialog from
-				reapearing on every page.
-			</p>
-			<p>
-				I use Google Analytics for statistics on my website. I like statistics because it tells me how many 
-				people visit my website and if any pages are more popular than others. This information is cruzial to provide
-				improvements where needed. 
-				<a href="https://developers.google.com/analytics/devguides/collection/analyticsjs/cookie-usage" target="_blank">Google Analytics use cookies</a>
-				to provide me with this information. I do not allow Google to share this information with third parties.
-			</p>
-
-			<h2>Privacy</h2>
-			<p>
-				I respect your privacy, because I want others to respect mine. I do not share any information with
-				third parties or sell any information related to you or your use of my website. Any data you submit
-				through this website will be stored securely on my server. Passwords and sensitive data will be stored
-				in encrypted form. I will delete your data upon request.
-			</p>
-
-			<h2>Terms of use</h2>
-			<p>
-				This website is offered as is. I cannot be held responsible for
-				content submitted by my users, or consequences derived, directly or indirectly, from using this website
-				or any ideas posted on the website.
-			</p>
-			<p>
-				I do not include Facebook sharing or other external plugins, which could lead to compromising your privacy.
-			</p>
-			<p>
-				I maintain the right to update or change these terms as needed.
-			</p>
-
-			<h2>Licenses</h2>
-			<p>
-				The content of this website is licensed under a 
-				<a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
-			</p>
-			<p>
-				That means everything you read on this website is protected
-				by copyright laws, but you can freely quote or reproduce any part of our content as long as you clearly state
-				its origin or provide a link to this website in direct conjonction with the reproduced text.
-			</p>
-			<p>
-				The source code of this website is licensed under a 
-				<a rel="license" href="https://github.com/kaestel/kaestel_dk/blob/master/LICENSE-CPAL.md">CPAL-1.0 licence</a>, 
-				and available as an open source project on <a rel="license" href="https://github.com/kaestel/kaestel_dk">Github</a>.
-			</p>
+			<?= $page_item["html"] ?>
 		</div>
+		<? endif; ?>
+
 	</div>
+
+
+
+<? else:?>
+
+	<h1>Cookies, privacy, terms of use and licenses</h1>
+	<p>Shoot. You caught us in the middle of an update. Try again later.</p>
+
+<? endif; ?>
+
 </div>
