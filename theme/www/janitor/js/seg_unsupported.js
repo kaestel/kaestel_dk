bebb75c8f0f787387913e5bd846c106ec593a52c
Martin Kæstel Nielsen <martin@think.dk>
2020-03-23 11:54:46 +0100

    build

--- theme/www/janitor/js/seg_unsupported.js
+++ theme/www/janitor/js/seg_unsupported.js
@@ -1,5 +1,5 @@
 /*
-asset-builder @ 2020-03-22 22:10:39
+asset-builder @ 2020-03-23 11:54:23
 */
 
 /*seg_unsupported_include.js*/
--- theme/www/js/seg_smartphone.js
+++ theme/www/js/seg_smartphone.js
@@ -1,5 +1,5 @@
 /*
-asset-builder @ 2020-03-22 22:10:44
+asset-builder @ 2020-03-23 11:54:19
 */
 
 /*seg_smartphone_include.js*/
