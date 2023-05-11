701aa94970a683538cb9b0281d7768fa2cc995f4
kaestel <martin@think.dk>
2023-05-11 10:03:12 +0200

    Update GA ID

--- theme/www/js/lib/u-settings.js
+++ theme/www/js/lib/u-settings.js
@@ -1,7 +1,7 @@
 u.site_name = "Æ";
 u.terms_version = "terms_v1";
 
-u.ga_account = 'UA-17394677-1';
+u.ga_account = 'G-J8YSKFWL76';
 u.ga_domain = 'kaestel.dk';
 u.gapi_key = "AIzaSyCrwT53aMp7GdH-GmgplFris4rAJzKGP3o";
 
