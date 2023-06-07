07cc2c35dd1ec80ddf7f198f07f2d66ba7ffcbea
kaestel <martin@think.dk>
2023-06-07 14:24:13 +0200

    Updated key

--- theme/www/js/lib/u-settings.js
+++ theme/www/js/lib/u-settings.js
@@ -3,6 +3,6 @@ u.terms_version = "terms_v1";
 
 u.ga_account = 'G-J8YSKFWL76';
 u.ga_domain = 'kaestel.dk';
-u.gapi_key = "AIzaSyCrwT53aMp7GdH-GmgplFris4rAJzKGP3o";
+u.gapi_key = "AIzaSyBMB089UoM8jopyT_dHW_eLPIcqp9wHrmk";
 
 u.txt["terms-headline"] = "I love <br />cookies and privacy";
